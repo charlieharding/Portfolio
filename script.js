83e245a942183c3bf74fc81aba33ca48760304c1
Charlie <mail@charlieharding.net>
2017-01-06 21:24:20 +1300

    Footer hover effect (half)

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
 window.onload = function(){
 
 	var proj_wrap, con_wrap, proj_link, con_link, proj_li, con_li, proj_close_svg, con_close_svg
+	var apollo_hov, drive_hov, david_hov, cod_hov; 
 
 	function setElements(){
+		//Main nav
 		proj_link = document.getElementById("project-link");
 		proj_wrap = document.getElementById("project-wrap");
 		proj_li = document.getElementById("proj-li");	
@@ -15,6 +17,41 @@ window.onload = function(){
 		con_close_svg = document.getElementById("contact-close-svg");
 		con_link.onclick = openContact;
 		con_close_svg.onclick = closeProject;
+
+		//Footer hovers
+		apollo_hov = document.getElementById("apollo-hov");
+		drive_hov = document.getElementById("drive-hov");
+		david_hov = document.getElementById("david-hov");
+		cod_hov = document.getElementById("cod-hov");
+
+		apollo_hov.addEventListener("mouseenter", function(event){
+			hoverIn(apollo_hov);
+		});
+		apollo_hov.addEventListener("mouseout", function(event){
+			hoverOut(apollo_hov);
+		});
+
+		drive_hov.addEventListener("mouseenter", function(event){
+			hoverIn(drive_hov);
+		});
+		drive_hov.addEventListener("mouseout", function(event){
+			hoverOut(drive_hov);
+		});
+
+		david_hov.addEventListener("mouseenter", function(event){
+			hoverIn(david_hov);
+		});
+		david_hov.addEventListener("mouseout", function(event){
+			hoverOut(david_hov);
+		});
+
+		cod_hov.addEventListener("mouseenter", function(event){
+			hoverIn(cod_hov);
+		});
+		cod_hov.addEventListener("mouseout", function(event){
+			hoverOut(cod_hov);
+		});
+
 	}
 
 	function openProject() {
@@ -41,7 +78,13 @@ window.onload = function(){
 		con_li.className = "";
 	}
 
+	function hoverIn(el){
+		el.parentElement.className = 'hov-on';
+	}
 
+	function hoverOut(el){
+		el.parentElement.className = '';
+	}
 
 	setElements();
 	//console.log('text')
